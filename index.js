bf0b48da84aff44d24a13895ec3d0baee36e8d96
codebuilderen-source <codebuilderen@gmail.com>
2026-04-13 10:59:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-// POST 요청을 처리하기 위한 미들웨어 설정 (index.js 상단)
-app.use(express.urlencoded({ extended: true }));
-
-// 사용자 추가 라우트
-app.post('/add-user', async (req, res) => {
-  const { username } = req.body;
-  try {
-    // Neon DB에 데이터 삽입 (테이블명이 users라고 가정)
-    await pool.query('INSERT INTO users (name) VALUES ($1)', [username]);
-    // 성공 후 메인 페이지로 다시 이동(새로고침 효과)
-    res.redirect('/');
-  } catch (err) {
-    console.error(err);
-    res.send("에러 발생");
+@@ -6,8 +6,9 @@
+  "scripts": {
+    "start": "node index.js"
+  },
+"dependencies": {
+  "express": "^4.18.2",
+  "pg": "^8.11.3",
+  "ejs": "^3.1.9"
   }
-});
+}
