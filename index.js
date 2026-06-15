d2fee31e0dfd00eeb72cd443daf2bab31292f5e9
codebuilderen-source <codebuilderen@gmail.com>
2026-06-15 10:19:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -335,3 +335,7 @@ app.post('/comment/:id/delete', async (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
